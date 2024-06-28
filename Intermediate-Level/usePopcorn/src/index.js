326bd2a200fbf89090f4f14cf89adea3c3e78978
Tarun Kumar Choudhary <tarunkc22@gmail.com>
2024-06-27 20:33:47 -0400

    Add files via upload

---
+++ Intermediate-Level/usePopcorn/src/index.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [myRating, setMyRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMyRating} />
+      <p>My rating : {myRating}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
+  </React.StrictMode>
+);
