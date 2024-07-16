68d68fdffa9329602656a25f3875dfbd9fdea0fe
Tarun Kumar Choudhary <tarunkc22@gmail.com>
2024-07-16 13:56:52 -0400

    Add files via upload

---
+++ Advance-Level/react-quiz/src/components/FinishScreen.js
@@ -0,0 +1,19 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const perc = (points / maxPossiblePoints) * 100;
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(perc)})
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+export default FinishScreen;
